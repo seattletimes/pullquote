10907bcd436c56d97da41af5cb3d1797913ec36f
Thomas Wilburn <news@thomaswilburn.net>
2017-08-14 10:06:25 -0700

    Added an attribution size control

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 var canvas = document.querySelector(".preview");
 var context = canvas.getContext("2d");
 
+var formatting = document.querySelector(".formatting");
 var download = document.querySelector("a.download");
-var textarea = document.querySelector("textarea");
+var textarea = document.querySelector("textarea.quote");
 textarea.value = textarea.value.trim();
+var attribution = document.querySelector("input.attribution")
 
 var bug = new Image();
 bug.src = "ST.svg";
@@ -41,29 +43,33 @@ var sizes = {
 
 var getSettings = function() {
   var settings = {};
-  var inputs = document.querySelectorAll("input[type=text], input[type=number], input[type=range], select");
+  var inputs = formatting.querySelectorAll("input[type=text], input[type=number], input[type=range], select");
   for (var i = 0; i < inputs.length; i++) {
     var input = inputs[i];
     settings[input.id] = input.value;
   }
-  var checkboxes = document.querySelectorAll("input:checked");
+  var checkboxes = formatting.querySelectorAll("input:checked");
   for (var i = 0; i < checkboxes.length; i++) {
     var box = checkboxes[i];
     settings[box.name] = box.value;
   }
   settings.size *= 1;
   settings.padding *= 1;
+  settings.attributionSize *= 1;
   return settings;
 };
 
-var layoutText = function(text, maxWidth) {
+var layoutText = function(text, maxWidth, font, size) {
   var lines = [];
   var position = 0;
   var buffer = "";
-  while (position < text.length) {
+  context.font = `${size}px ${font}`;
+  var die = 0;
+  while (position < text.length && die < 10000) {
+    die++;
     var char = text[position];
     if (char == "\n") {
-      lines.push({ text: buffer, width: context.measureText(buffer).width });
+      lines.push({ text: buffer, width: context.measureText(buffer).width, size });
       position++;
       buffer = "";
       continue;
@@ -72,21 +78,24 @@ var layoutText = function(text, maxWidth) {
     buffer += char;
     var metric = context.measureText(buffer);
     if (metric.width > maxWidth) {
-      var words = buffer.split(" ");
+      var words = buffer.trim().split(" ");
       if (words.length > 1) {
         var last = words.pop();
-        position -= last.length + 1;
+        if (context.measureText(last).width < maxWidth) {
+          //check for overrun
+          position -= last.length + 1;
+        }
       } else {
         position++;
       }
       words = words.join(" ");
-      lines.push({ text: words, width: context.measureText(words).width });
+      lines.push({ text: words, width: context.measureText(words).width, size });
       buffer = "";
     }
     position++;
   }
   if (buffer) {
-    lines.push({ text: buffer, width: context.measureText(buffer).width });
+    lines.push({ text: buffer.trim(), width: context.measureText(buffer).width, size });
   }
   return lines;
 };
@@ -159,27 +168,33 @@ var render = function() {
   
   //lay out the text
   context.fillStyle = fg[settings.theme] || fg.light;
-  context.font = `${settings.size}px ${settings.font}`;
   var padding = settings.padding;
   var maxWidth = canvas.width - padding * 2;
-  var lines = layoutText(text, maxWidth);
+  var lines = layoutText(text, maxWidth, settings.font, settings.size);
+  var attrLines = layoutText("    — " + attribution.value.trim(), maxWidth, settings.font, settings.attributionSize);
+  attrLines.forEach(l => l.alignment = "right");
+  lines = lines.concat(attrLines);
+  var height = lines.reduce((t, l) => t + l.size, 0);
   
   //draw the text
-  var lineY = canvas.height / 2 + settings.size / 2 - lines.length / 2 * settings.size;
+  var lineY = canvas.height / 2 - height / 2 + settings.size / 2;
   if (settings.alignY == "top") {
     lineY = padding + settings.size;
   } else if (settings.alignY == "bottom") {
-    lineY = canvas.height - lines.length * settings.size - padding;
+    lineY = canvas.height - height - padding + settings.size / 2;
   }
   lines.forEach(function(l) {
     var x = padding;
-    if (settings.alignX == "right") {
-      x = canvas.width - l.width - padding;
-    } else if (settings.alignX == "center") {
-      x = canvas.width / 2 - l.width / 2;
+    var alignment = l.alignment || settings.alignX;
+    if (alignment == "right") {
+      x = canvas.width - padding;
+    } else if (alignment == "center") {
+      x = canvas.width / 2;
     }
+    context.textAlign = alignment;
+    context.font = `${l.size}px ${settings.font}`;
     context.fillText(l.text, x, lineY);
-    lineY += settings.size;
+    lineY += l.size;
   });
 };
 
